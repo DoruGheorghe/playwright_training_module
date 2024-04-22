9619bf58900f624b4eae2ae33d0fad658b25fcf3
DoruGheorghe <doru.gheorghe@endava.com>
2024-04-22 15:52:46 +0300

    Added first Contact page test

--- tests/contactUsPage.test.ts
+++ tests/contactUsPage.test.ts
@@ -1,3 +1,23 @@
 import { test, expect, chromium } from '@playwright/test';
 import { setupBrowser } from '../test_utils/chromeBrowserSetup.ts';
 import { fillAndSubmitFormContactPage, chooseFile, submitContactForm } from '../pages/contacUsPage.ts';
+
+
+test('Submit message', async () => {
+
+    const { browser, context, page } = await setupBrowser();
+    await page.goto('./');
+    const locator = page.getByText("Contact us");
+    await locator.click();
+    fillAndSubmitFormContactPage(page, 'wa', 'wa@wa.com', 'something', 'something something')
+    await page.waitForTimeout(5000);
+    chooseFile(page);
+    await page.waitForTimeout(5000);
+    page.once('dialog', (dialog) =>{
+        console.log(dialog.message)
+        dialog.accept();
+    });
+    await page.waitForTimeout(5000);
+    submitContactForm(page);
+    await page.waitForTimeout(5000);
+});
